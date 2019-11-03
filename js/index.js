8c5b771823e85588d2ba3896928d819662fb4048
Julian Joseph <julianjoseph52@gmail.com>
2019-11-03 00:21:36 -0400

    Added Additional Pages and Javascript

---
+++ js/index.js
@@ -0,0 +1,15 @@
+function webDev(){
+  window.location.replace("web.html");
+}
+
+function mobileDev(){
+  window.location.replace("mobile.html");
+}
+
+function softwareDev(){
+  window.location.replace("software.html");
+}
+
+function machineLearning(){
+  window.location.replace("machine_learning.html");
+}
